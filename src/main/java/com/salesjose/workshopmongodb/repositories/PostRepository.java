754fcca44de0d0246508b24c36521689af992465
Victor <josvictoronofre@gmail.com>
2024-04-10 10:13:10 -0300

    Entity post with agregated user

--- src/main/java/com/salesjose/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/salesjose/workshopmongodb/repositories/PostRepository.java
@@ -3,9 +3,9 @@ package com.salesjose.workshopmongodb.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.salesjose.workshopmongodb.domain.User;
+import com.salesjose.workshopmongodb.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 }
