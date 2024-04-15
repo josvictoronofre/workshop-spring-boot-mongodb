38db819bda25578f652b5938a9e968c7536ba0f1
Victor <josvictoronofre@gmail.com>
2024-04-15 08:58:57 -0300

    Post findById

---
+++ src/main/java/com/salesjose/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.salesjose.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.salesjose.workshopmongodb.domain.Post;
+import com.salesjose.workshopmongodb.repositories.PostRepository;
+import com.salesjose.workshopmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+}
